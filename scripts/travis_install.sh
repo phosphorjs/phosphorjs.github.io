16128f79a448acf2f5bddd1e1d29412f6a0a9589
Steven Silvester <steven.silvester@ieee.org>
2015-09-22 15:15:35 -0500

    Move the example build to travis_script

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 git checkout source
 npm install
-
-cd examples
-for D in *; do
-   cd $D
-   npm install && npm run build
-   cd ..
-done
--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 node index.js
+
+cd examples
+for D in *; do
+   cd $D
+   npm install && npm run build
+   cd ..
+done
+cd ..
 cp -r examples build
