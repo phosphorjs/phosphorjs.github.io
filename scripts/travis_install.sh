ba7b5ab9b135e6585370d8e87e3f59d3551e6ed1
Steven Silvester <steven.silvester@ieee.org>
2015-09-22 10:15:36 -0500

    Move the example build to travis_script

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 git checkout source
 npm install
-git clone https://github.com/blink1073/todomvc-demo
-cd todomvc-demo
-npm install
-npm run build
-cp LICENSE example
-mkdir ../build/todo-example
-cp -r example ../build/todo-example
--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 node index.js
+git clone https://github.com/blink1073/todomvc-demo
+cd todomvc-demo
+npm install
+npm run build
+cp LICENSE example
+mkdir ../build/todo-example
+cp -r example ../build/todo-example
