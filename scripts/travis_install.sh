89b8513b015f83282d4d1fa59628cd3a9cce4c42
Steven Silvester <steven.silvester@ieee.org>
2015-09-22 10:09:35 -0500

    Add the todo mvc example

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 git checkout source
 npm install
+git clone https://github.com/blink1073/todomvc-demo
+cd todomvc-demo
+npm install
+npm run build
+cp LICENSE example
+cp -r example ../build/todo-example
