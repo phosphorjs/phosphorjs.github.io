e27cbf85a7e09a3ff577df654b0a607f2ecfa839
Steven Silvester <steven.silvester@ieee.org>
2015-09-24 10:33:39 -0500

    wip tweaking css

--- examples/dockpanel/index.ts
+++ examples/dockpanel/index.ts
@@ -8,12 +8,12 @@
 'use strict';
 
 import {
-  Message
-} from 'phosphor-messaging';
+  DockPanel
+} from 'phosphor-dockpanel';
 
 import {
-  SplitPanel
-} from 'phosphor-splitpanel';
+  Message
+} from 'phosphor-messaging';
 
 import {
   Tab, TabPanel
@@ -24,46 +24,6 @@ import {
 } from 'phosphor-widget';
 
 import './index.css';
-
-
-// This widget uses flexbox for layout: see ./index.css
-class MyVBox extends Widget {
-
-  constructor() {
-    super();
-    this.addClass('my-vbox');
-  }
-}
-
-
-class MyResizeWidget extends Widget {
-  // All widgets will receive a resize message when their parent
-  // determines that they have likely been resized. If the current
-  // size of the widget is known, it will be passed as part of the
-  // message. Otherwise, the size parameters will be `-1`, and you
-  // will need to measure the node to get the current size.
-  //
-  // The current size will typically be known when the parent of
-  // the widget is an absolute Phosphor layout panel, and will be
-  // unknown when the parent is a widget which uses CSS to layout
-  // its children. Here is a link to the typical way to handle the
-  // condition in the most efficient way possible, by only measuring
-  // if required:
-  // https://github.com/phosphorjs/phosphor-splitpanel/blob/master/src/index.ts#L368
-  protected onResize(msg: ResizeMessage): void {
-    var w = msg.width;
-    var h = msg.height;
-    console.log(this.node.className, 'width:', w, 'height:', h);
-  }
-}
-
-
-function createContent(name: string): Widget {
-  var widget = new MyResizeWidget();
-  widget.addClass('content');
-  widget.addClass(name);
-  return widget;
-}
 
 
 /**
@@ -105,23 +65,53 @@ class CodeMirrorWidget extends Widget {
 }
 
 
+function createContent(title: string): Widget {
+  var widget = new Widget();
+  widget.addClass('content');
+  widget.addClass(title.toLowerCase());
+
+  var tab = new Tab(title);
+  tab.closable = true;
+  DockPanel.setTab(widget, tab);
+
+  return widget;
+}
+
+
 function main(): void {
-  var r = createContent('red');
-  var y = createContent('yellow');
-  var g = createContent('green');
+  var r1 = createContent('Red');
+  var r2 = createContent('Red');
+  var r3 = createContent('Red');
 
-  var b1 = createContent('blue');
-  var b2 = createContent('blue');
-  var b3 = createContent('blue');
+  var b1 = createContent('Blue');
+  var b2 = createContent('Blue');
+  var b3 = createContent('Blue');
 
-  var split = new SplitPanel();
-  split.children = [b1, b2, b3];
+  var g1 = createContent('Green');
+  var g2 = createContent('Green');
+  var g3 = createContent('Green');
 
-  // Note that the flexbox is the root widget, not the split panel.
-  // Since it is in a Phosphor layout, when the window resizes, 
-  // the flexbox's decendants get the resize message they need.
-  var box = new MyVBox();
-  box.children = [r, split, y, g];
+  var y1 = createContent('Yellow');
+  var y2 = createContent('Yellow');
+  var y3 = createContent('Yellow');
+
+  var panel = new DockPanel();
+
+  panel.addWidget(r1);
+
+  panel.addWidget(b1, DockPanel.SplitRight, r1);
+  panel.addWidget(y1, DockPanel.SplitBottom, b1);
+  panel.addWidget(g1, DockPanel.SplitLeft, y1);
+
+  panel.addWidget(b2, DockPanel.SplitBottom);
+
+  panel.addWidget(y2, DockPanel.TabBefore, r1);
+  panel.addWidget(b3, DockPanel.TabBefore, y2);
+  panel.addWidget(g2, DockPanel.TabBefore, b2);
+  panel.addWidget(y3, DockPanel.TabBefore, g2);
+  panel.addWidget(g3, DockPanel.TabBefore, y3);
+  panel.addWidget(r2, DockPanel.TabBefore, b1);
+  panel.addWidget(r3, DockPanel.TabBefore, y1);
 
   // Create the CodeMirror widget with a typescript mode.
   var cmSource = new CodeMirrorWidget({
@@ -138,17 +128,17 @@ function main(): void {
   });
   cmCss.loadTarget('./index.css');
 
-  TabPanel.setTab(box, new Tab('Demo'));
+  TabPanel.setTab(panel, new Tab('Demo'));
   TabPanel.setTab(cmSource, new Tab('Source'));
   TabPanel.setTab(cmCss, new Tab('CSS'));
 
-  var panel = new TabPanel()
-  panel.id = 'main';
-  panel.widgets = [box, cmSource, cmCss];
+  var tabs = new TabPanel()
+  tabs.id = 'main';
+  tabs.widgets = [panel, cmSource, cmCss];
 
-  attachWidget(panel, document.body);
+  attachWidget(tabs, document.body);
 
-  window.onresize = () => panel.update();
+  window.onresize = () => tabs.update();
 }
 
 
