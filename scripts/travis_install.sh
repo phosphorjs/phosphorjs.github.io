8da03179f2b6949697da731b2f1199a2ae79e45f
S. Chris Colbert <sccolbert@gmail.com>
2017-02-25 18:27:59 -0600

    disable examples and docs for now

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git checkout source
 
-cd examples
-npm install
+# cd examples
+# npm install
--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -5,11 +5,11 @@ mkdir -p build
 
 cp -r index-redux/* build/.
 
-cd examples
-npm run build
-cd ..
+# cd examples
+# npm run build
+# cd ..
 
-mkdir -p build/examples
-for D in $( ls -d examples/*/ | grep -v node_modules ); do
-	cp -r $D ./build/$D
-done
+# mkdir -p build/examples
+# for D in $( ls -d examples/*/ | grep -v node_modules ); do
+# 	cp -r $D ./build/$D
+# done
